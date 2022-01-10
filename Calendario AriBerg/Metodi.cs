99694ba936ec97db5b331346148db90c9136f8ad
Alessandro Giuseppe Gioia <alessandroogioiaa@gmail.com>
2022-01-10 17:40:54 +0100

    dc

---
+++ Calendario AriBerg/Metodi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Calendario_AriBerg
+{
+    internal class Metodi
+    {
+        private MySqlConnection ConnectToDatabase(string hostname, string dbname, string username, string password)
+        {
+            string remoteConnectionString = $"Server={hostname}; Database={dbname}; Uid={username}; Pwd={password};";
+            MySqlConnection conn = new MySqlConnection(remoteConnectionString);
+            conn.Open();
+            return conn;
+        }
+
+        private MySqlConnection ConnectToDatabase()
+        {
+            string remoteConnectionString = $"Server=/; Database=/; Uid=/; Pwd=/;";
+            MySqlConnection conn = new MySqlConnection(remoteConnectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
